e15f2826d95ac6811765b02a85ec698e111a88df
jettnipith <35611274+jettnipith@users.noreply.github.com>
2023-11-25 23:09:47 +0700

    Update relayExample.py

--- relayExample.py
+++ relayExample.py
@@ -13,19 +13,22 @@ def relay_off():
     relay_pin.value(0)
 
 # Main loop
-while True:
-    try:
-        # Turn the relay on
-        relay_on()
-        print('Relay is ON')
-        time.sleep(2)
-
-        # Turn the relay off
-        relay_off()
-        print('Relay is OFF')
-        time.sleep(2)
-
-    except Exception as e:
-        print(f'Error controlling relay module: {e}')
-
-    time.sleep(2)  # Adjust the delay based on your application requirements
+try:
+    while True:
+        try:
+            # Turn the relay on
+            relay_on()
+            print('Relay is ON')
+            time.sleep(2)
+    
+            # Turn the relay off
+            relay_off()
+            print('Relay is OFF')
+            time.sleep(2)
+    
+        except Exception as e:
+            print(f'Error controlling relay module: {e}')
+    
+        time.sleep(2)  # Adjust the delay based on your application requirements
+except KeyboardInterupt:
+    pass
