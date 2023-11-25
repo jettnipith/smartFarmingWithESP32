1587c0d669470cab729c657a855d1f0b95fc91a4
jettnipith <35611274+jettnipith@users.noreply.github.com>
2023-11-25 22:05:59 +0700

    Update dht11Example.py

--- dht11Example.py
+++ dht11Example.py
@@ -10,17 +10,20 @@ import time
 sensor = dht.DHT11(machine.Pin(2))  # Replace with the GPIO pin you connected the sensor to
 
 # Main loop
-while True:
-    try:
-        # Read sensor data
-        sensor.measure()
-        temperature = sensor.temperature()
-        humidity = sensor.humidity()
-
-        # Print sensor data
-        print(f'Temperature: {temperature}°C, Humidity: {humidity}%')
-
-    except Exception as e:
-        print(f'Error reading sensor data: {e}')
-
-    time.sleep(2)  # Adjust the delay based on your application requirements
+try:
+    while True:
+        try:
+            # Read sensor data
+            sensor.measure()
+            temperature = sensor.temperature()
+            humidity = sensor.humidity()
+    
+            # Print sensor data
+            print(f'Temperature: {temperature}°C, Humidity: {humidity}%')
+    
+        except Exception as e:
+            print(f'Error reading sensor data: {e}')
+    
+        time.sleep(2)  # Adjust the delay based on your application requirements
+except KeyboardInterupt:
+    pass
