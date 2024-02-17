9e23f3188bff087b0a85634bb80712c04494dbbe
jettnipith <35611274+jettnipith@users.noreply.github.com>
2024-02-17 12:15:41 +0700

    Update preBoot.py

--- preBoot.py
+++ preBoot.py
@@ -1,18 +1,20 @@
 import analogSense
 import digitalSense
+import actuators
 
 #Mian Program
 try:
     while True:
         soil = analogSense.moisture()
         air = digitalSense.dht11()
+        #prevent error from dht11
         if air["error"] is None:
             print(f'moisture: {soil["percent"]}')
             print(f'temp: {air["temperature"]}')
             print("----------")
-        
-        
-
+            if soil["percent"] < 5:
+                actuators.relayOn(1)
+            else:
+                actuators.relayOff(1)
 except KeyboardInterupt:
         pass
-
