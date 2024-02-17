c95b02f401b319c9faffc76b9ac10417f9ce6c96
jettnipith <35611274+jettnipith@users.noreply.github.com>
2024-02-17 11:48:09 +0700

    Update preBoot.py

--- preBoot.py
+++ preBoot.py
@@ -1,13 +1,16 @@
 import analogSense
-
-
-
+import digitalSense
 
 #Mian Program
 try:
     while True:
         soil = analogSense.moisture()
-        print(f'moisture: {soil["percent"]}')
+        air = digitalSense.dht11()
+        if air["error"] is None:
+            print(f'moisture: {soil["percent"]}')
+            print(f'temp: {air["temperature"]}')
+            print("----------")
+        
         
 
 except KeyboardInterupt:
