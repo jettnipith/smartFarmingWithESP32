2582b0570b5a5468eba3be3d4e9888295072640b
jettnipith <35611274+jettnipith@users.noreply.github.com>
2024-03-02 13:28:59 +0700

    Update sheetAPI.js

--- sheetAPI.js
+++ sheetAPI.js
@@ -2,9 +2,13 @@ var ss = SpreadsheetApp.openById('1mICyEsokUMQX_rj6r0ofmFkeLQ2zqangyxB4ILv47c8')
 var sheet = ss.getSheetByName('ชีต1')
 
 function doGet(e) {
-  var action = e.parameter.action
-if (action == 'getAllData') {
-    return getAllData(e)
+  var action = e.parameter.action;
+  if (action == 'getAllData') {
+    return getAllData(e);
+  } else if (action == 'createNewData') {
+    return createNewData(e);
+  } else if (action == 'updateData') {
+    return updateData(e);
   }
 }
 
@@ -22,4 +26,32 @@ function getAllData(e) {
   }
   var result = JSON.stringify(data)
   return ContentService.createTextOutput(result).setMimeType(ContentService.MimeType.JSON)
+}
+
+function createNewData(e) {
+  var deviceId = e.parameter.deviceId;
+  var deviceType = e.parameter.deviceType;
+  var deviceName = e.parameter.deviceName;
+  var deviceVal = e.parameter.deviceVal;
+  
+  // Assuming the data will be added to the next available row
+  var newRow = [deviceId, deviceType, deviceName, deviceVal];
+  sheet.appendRow(newRow);
+  
+  return ContentService.createTextOutput("Data created successfully").setMimeType(ContentService.MimeType.TEXT);
+}
+
+function updateData(e) {
+  var rowToUpdate = e.parameter.row; // Assuming this is the row number to update
+  var deviceId = e.parameter.deviceId;
+  var deviceType = e.parameter.deviceType;
+  var deviceName = e.parameter.deviceName;
+  var deviceVal = e.parameter.deviceVal;
+
+  // Update the values in the specified row
+  var range = sheet.getRange(rowToUpdate, 1, 1, 4); // Assuming 4 columns
+  var newValues = [deviceId, deviceType, deviceName, deviceVal];
+  range.setValues([newValues]);
+  
+  return ContentService.createTextOutput("Data updated successfully").setMimeType(ContentService.MimeType.TEXT);
 }
